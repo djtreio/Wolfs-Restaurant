498e29628e9313299c1b74c9fe7abac824ea15cb
bsmith5142 <36649786+bsmith5142@users.noreply.github.com>
2020-08-04 16:15:06 -0400

    Update MenuController.java
    Added some documentation

--- MenuController.java
+++ MenuController.java
@@ -13,19 +13,26 @@ public class MenuController {
 	}
 	
 	//Changes status of an item to y(meaning on menu)
+	//Params: Item to add to menu
+	//Returns: void
 	public static void addToMenu(MenuType item) {
+		//Change status of item
 		item.editItem("status", "y");
+		//Add to menu
 		MenuDatabase.addMenuItem(item);
-		
-		//Add newitem to GUI(Jackson, let me know when you get here I'll help implement this. Not sure how do to it since you've made the GUI
 		
 	}
 	
+	//Sets the variables of an item on the menu
+	//Params: Item to set variables of
+	//Returns: void
 	public static void setMenuItem(MenuType item) {
+		//Check if item is not on menu
 		if (menuid == -1) {
 			addToMenu(item);
 
 		}
+		//Edit the variabels
 		else {
 			item.editItem("name", item.name);
 			item.editItem("category", item.category);
@@ -33,11 +40,16 @@ public class MenuController {
 		}
 	}
 	
+	//Removes an item from the menu
+	//Params: Item to remove from menu
+	//Returns: void
 	public static void removefromMenu(MenuType item) {
 		item.editItem("status", "n");
-		//Fix GUI
 	}
 	
+	//Retrieves list of items that should be on menu
+	//Params: None
+	//Returns: array of MenuType items, containing menu items
 	public static MenuType[] getMenu() {
 		MenuType[] menuItems = MenuDatabase.getMenuItems();
 		return menuItems;
